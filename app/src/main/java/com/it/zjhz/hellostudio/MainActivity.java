174bbe4ed57f0de9799733bcd1348ddb253b171e
hangzhou <18268883283@163.com>
2016-06-06 16:23:18 +0800

    走马

--- app/src/main/java/com/it/zjhz/hellostudio/MainActivity.java
+++ app/src/main/java/com/it/zjhz/hellostudio/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("原谅我这一首不为谁而作的歌");
+        System.out.println("过了很久终于我愿抬头看");
     }
 }
